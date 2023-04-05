cc9f016fb8be408a283ac9311f092e3f48831c82
DKreutz0 <79404367+DKreutz0@users.noreply.github.com>
2023-04-05 12:33:58 +0200

    trimTrailingWhitespace

--- VMWareWorkstation-API/Examples/Create-Cloned-VM.ps1
+++ VMWareWorkstation-API/Examples/Create-Cloned-VM.ps1
@@ -1,5 +1,5 @@
 <# Create a Template
-    # 
+    #
     # https://github.com/DKreutz0/VMWareWorkstationAPI/blob/main/VMWareWorkstation-API/Documentation/Create%20a%20VM%20template%20on%20VMware%20Workstation.docx
     # https://github.com/DKreutz0/VMWareWorkstationAPI/blob/main/VMWareWorkstation-API/Documentation/Create%20a%20VM%20template%20on%20VMware%20Workstation.docx
     #
@@ -8,7 +8,7 @@
 try {
     Import-Module -Name VMWareWorkstation-API -ErrorAction Stop
     [string]$VMTemplate = "649TJ74BEAHCM93M56DM79CD21562M8D" # Get your VM Id with the Get-VMTemplates function
-    #Start creation of the Virtual Machine    
+    #Start creation of the Virtual Machine
 
         $NewVMCloneName = $("CLONEDVM-" + -join ((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count 11 | ForEach-Object  {[char]$_})).ToUpper()
         $NewClonedVM = New-VMClonedMachine -NewVMCloneName $NewVMCloneName -NewVMCloneId $VMTemplate -ResponseDetails -ErrorAction Stop
@@ -19,7 +19,7 @@ try {
 
         $ClonePath = Get-VirtualMachines -VirtualMachineName $NewVMCloneName
         $RegisterVM = Register-VMClonedMachine -NewVMCloneName $NewVMCloneName -VMClonePath $ClonePath.path -ResponseDetails -ErrorAction Stop
-    
+
     $RegisterVM
 
 }
